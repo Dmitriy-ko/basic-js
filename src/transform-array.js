3bd084b1a7a2c3df6e79477b9d3b9273d5e3116e
Dmitriy-ko <79768841+Dmitriy-ko@users.noreply.github.com>
2021-03-29 23:28:21 +0300

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!(arr instanceof Array)) throw new Error("Error");
+  if (!arr.some(isTransform)) return arr;
+  function isTransform(item) {
+    return (
+      item === "--discard-prev" ||
+      item === "--discard-next" ||
+      item === "--double-next" ||
+      item === "--double-prev"
+    );
+  }
+
+  let result = [];
+
+  arr.forEach((item, i, arr) => {
+    switch (item) {
+      case "--discard-next":
+        break;
+      case "--discard-prev":
+        if (result.length && arr[i - 2] !== "--discard-next") {
+          result.pop();
+        }
+        break;
+      case "--double-next":
+        if (i !== arr.length - 1) {
+          result.push(arr[i + 1]);
+        }
+        break;
+      case "--double-prev":
+        if (i !== 0 && arr[i - 2] !== "--discard-next") {
+          result.push(arr[i - 1]);
+        }
+        break;
+      default:
+        if (arr[i - 1] !== "--discard-next") {
+          result.push(item);
+        }
+        break;
+    }
+  });
+
+  return result;
 };
+
